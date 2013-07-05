b6680281e9da5f304b7a26f70d55f2bf08072b94
Arjan Scherpenisse <arjan@scherpenisse.net>
2013-07-05 15:42:36 +0200

    Updates for 2013

--- src/chat_client.erl
+++ src/chat_client.erl
@@ -1,24 +1,35 @@
-%% A simple chat server.
+%% A simple chat client.
 
 -module(chat_client).
 
--export([connect/2, send/2]).
+-export([connect/1, send/2, clientloop/1]).
 
--record(client, {pid, host}).
 
-connect(Name, Node) ->
-    ClientPid = spawn(fun() -> loop() end),
-    {Name, Node} ! {connect, ClientPid},
-    #client{pid=ClientPid, host={Name, Node}}.
+connect(Server) ->
+    spawn(fun() ->
+                  do_connect(Server) end).
 
-loop() ->
-    receive 
-        _Any ->
-            io:format("Received: ~p~n", [_Any]),
-            loop()
+
+do_connect(Server) ->
+    %% Send "connect" msg and wait for return
+    Server ! {connect, self()},
+    clientloop(Server).
+
+
+clientloop(Server) ->
+    receive
+        {send_to_server, Msg} ->
+            Server ! {message, Msg},
+            chat_client:clientloop(Server);
+
+        Something ->
+            io:format("Received something from the server"),
+            io:format("--> ~p~n", [Something]),
+            chat_client:clientloop(Server)
     end.
 
 
-send(#client{pid=Pid, host=Host}, Message) ->
-    Host ! {message, {Pid, Message}}.
-
+%% send a message from the erlang shell to the client process, which
+%% will send it to the server process.
+send(Client, Message) ->
+    Client ! {send_to_server, Message}.
--- src/chat_server.erl
+++ src/chat_server.erl
@@ -2,16 +2,38 @@
 
 -module(chat_server).
 
--export([start/1]).
+-export([start/0, loop/1]).
 
-start(Name) ->
+start() ->
     Pid = spawn(fun() -> loop([]) end),
-    register(Name, Pid).
+    register(server, Pid).
+
 
 loop(Clients) ->
-    receive 
-        _Any ->
-            io:format("Received: ~p~n", [_Any]),
-            loop(Clients)
+    receive
+
+        {connect, ClientPid} ->
+            io:format("A new client connects! ~p~n", [ClientPid]),
+            %% Add the ClientPid to the list of clients.
+            chat_server:loop( [ ClientPid|Clients] );
+
+        {message, MessageString} ->
+            send_message_to_clients(Clients, MessageString),
+            chat_server:loop(Clients);
+
+        SomeMessage ->
+            io:format("Received something unknown: ~p~n", [SomeMessage]),
+            chat_server:loop(Clients)
     end.
+
+
+%% @doc Send the message to each client.
+send_message_to_clients([], _MessageString) ->
+    %% nobody left to send to
+    ok;
+send_message_to_clients([Client | OtherClients], MessageString) ->
+    %% Send to the first client in the client list.
+    Client ! MessageString,
+    %% Send to the other clients.
+    send_message_to_clients(OtherClients, MessageString).
 
